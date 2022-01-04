c606afd194584c43ca911869eafbf360ebe44137
pigfall <koi_town@outlook.com>
2022-01-04 20:27:29 +0800

    clean

--- pf_ndk_examples/examples/hello.rs
+++
@@ -1,7 +0,0 @@
-use pf_ndk_glue::app::AndroidApp;
-use std::sync::{Arc,Mutex,Condvar};
-
-#[cfg_attr(target_os = "android", pf_ndk_glue::main())]
-pub fn run(arc_app_cond:Arc<(Mutex<AndroidApp>,Condvar)>) {
-    println!("hello world");
-}
--- pf_ndk_examples/src/lib.rs
+++
@@ -1,4 +0,0 @@
-#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
-pub fn main() {
-    println!("hello world");
-}
